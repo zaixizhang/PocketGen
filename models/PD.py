b3106eaf7b5472b42562299d970ef35b26b45e02
zaixizhang <52197241+zaixizhang@users.noreply.github.com>
2024-02-18 09:55:48 +0800

    Update PD.py

--- models/PD.py
+++ models/PD.py
@@ -376,7 +376,7 @@ class Pocket_Design_new(Module):
         return aar, rmsd
 
 
-def sample_from_categorical(logits=None, temperature=1.0):
+def sample_from_categorical(logits=None, temperature=2.0):
     if temperature:
         dist = torch.distributions.Categorical(logits=logits.div(temperature))
         tokens = dist.sample()
